0485879fccbe1262349c01da93c30ebd570dcfee
Goran Radenkovic <goran.cz@outlook.com>
2024-04-06 13:09:41 +0200

    Task1: RealCalculator ; Task 2: AverageNumber; Task 3: SwapNumbers

---
+++ Homework1/Task 3/Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the First Number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the Second Number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            int temp = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temp;
+
+            Console.WriteLine("After Swapping:");
+            Console.WriteLine($"First Number: {firstNumber}");
+            Console.WriteLine($"Second Number: {secondNumber}");
+        }
+    }
+}
