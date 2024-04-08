5c9c4e0992f7fcb43cd506f25f83592e4857fe24
Goran Radenkovic <goran.cz@outlook.com>
2024-04-08 19:34:46 +0200

    Sum Of Even & Student Group application

---
+++ Homework 2/StudentGroup/StudentGroup/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = { "Goran", "Milica", "Stefan", "Tamara", "Sava" };
+
+            Console.WriteLine("Enter student group: ( 1 or 2 )");
+            int groupNumber = Convert.ToInt32(Console.ReadLine());
+
+            switch (groupNumber)
+            {
+                case 1:
+                    Console.WriteLine("The Students in group 1 are:");
+                    foreach (string student in studentsG1)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("The Students in group 2 are:");
+                    foreach (string student in studentsG2)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid group number.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Homework 2/SumOfEven/SumOfEven/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Enter integer no.{i + 1}:");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+
+            Console.WriteLine($"The result is: {sum}");
+        }
+    }
+}
