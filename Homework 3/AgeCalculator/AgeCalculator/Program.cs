6d8835ccc02501732eb4c2f0960472df07cd7a53
Goran Radenkovic <goran.cz@outlook.com>
2024-04-12 20:12:15 +0200

    AgeCalculator & Substrings

---
+++ Homework 3/AgeCalculator/AgeCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AgeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your birthdate (format: yyyy-mm-dd): ");
+
+            string input = Console.ReadLine();
+
+            DateTime birthDate;
+            if (DateTime.TryParse(input, out birthDate))
+            {
+                int age = CalculateAge(birthDate);
+
+                Console.WriteLine($"You are {age} years old.");
+            }
+            else
+            {
+                Console.WriteLine("The date you entered is not valid. Please try again.");
+            }
+        }
+        static int CalculateAge(DateTime birthDate)
+        {
+            DateTime today = DateTime.Today;
+
+            int age = today.Year - birthDate.Year;
+
+            if (birthDate.Date > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+}
---
+++ Homework 3/Substrings/Substrings/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Substrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Substrings();
+        }
+
+        static void Substrings()
+        {
+            string str = "Hello from SEDC Codecademy 2024";
+            Console.WriteLine("Enter a number:");
+
+            int n;
+            bool isNumber = Int32.TryParse(Console.ReadLine(), out n);
+
+            if (!isNumber)
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+                return;
+            }
+
+            if (n > str.Length)
+            {
+                Console.WriteLine($"The number you entered is greater than the length of the string. The length of the string is {str.Length}.");
+                return;
+            }
+
+            string substring = str.Substring(0, n);
+            Console.WriteLine($"The first {n} characters from the string are: {substring}");
+            Console.WriteLine($"The length of the new string is: {substring.Length}");
+        }
+    }
+}
+
